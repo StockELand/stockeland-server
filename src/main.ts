201cfc92b59989dfe32cba86efc448cd7363ed7a
l11040 <113540683+l11040@users.noreply.github.com>
2025-02-01 22:00:33 +0900

    [Add] 임시 SSE 기능 추가 (#1)

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(); //TODO 추후 제약을 추가해야함
   await app.listen(8080);
 }
 bootstrap();
