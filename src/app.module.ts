ebce48b15a2a29d334451d4c1120b7454f84ac7b
l11040 <l1104033837@gmail.com>
2025-02-02 19:30:48 +0900

    [Add] DB 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SseModule } from './sse/sse.module';
+import { ParserModule } from './parser/parser.module';
+import { StockService } from './stock/stock.service';
+import { StockModule } from './stock/stock.module';
+import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [SseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DbModule,
+    SseModule,
+    ParserModule,
+    StockModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, StockService],
 })
 export class AppModule {}
