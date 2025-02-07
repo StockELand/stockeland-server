e630ea67c8fa92b608002d74a74f09dc6e9221ef
l11040 <l1104033837@gmail.com>
2025-02-07 14:50:21 +0900

    [Update] 예측 데이터 조회 추가

--- src/stock/stock.module.ts
+++ src/stock/stock.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { StockData } from 'src/entities/stock.entity';
 import { StockInfoData } from 'src/entities/stock-info.entity';
 import { StockPrediction } from 'src/entities/stock-prediction.entity';
+import { StockController } from './stock.controller';
 
 @Module({
   imports: [
@@ -11,5 +12,6 @@ import { StockPrediction } from 'src/entities/stock-prediction.entity';
   ],
   providers: [StockService],
   exports: [StockService, TypeOrmModule],
+  controllers: [StockController],
 })
 export class StockModule {}
