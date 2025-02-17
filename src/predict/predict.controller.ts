7aba208e6a343ed6ceb98505d2a0a02fa6555209
l11040 <l1104033837@gmail.com>
2025-02-17 17:54:48 +0900

    [Add] 예측 데이터 조회 추가

--- src/predict/predict.controller.ts
+++ src/predict/predict.controller.ts
@@ -13,6 +13,15 @@ export class PredictController {
     private readonly predictionLogService: PredictionLogService,
   ) {}
 
+  @Get('')
+  async getPredictedDataByDate(@Query('date') date: string) {
+    if (!date) {
+      return { message: '날짜를 YYYY-MM-DD 형식으로 입력하세요.' };
+    }
+
+    return await this.predictService.getPredictedDataByDate(date);
+  }
+
   /**
    * 특정 날짜의 로그 조회
    * @param date (YYYY-MM-DD)
