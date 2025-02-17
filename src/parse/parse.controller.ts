6e8278f8bafe2394661f90c3b936203c63686dc7
l11040 <l1104033837@gmail.com>
2025-02-17 17:45:37 +0900

    [Add] 파싱된 데이터 조회

--- src/parse/parse.controller.ts
+++ src/parse/parse.controller.ts
@@ -13,6 +13,15 @@ export class ParseController {
     private readonly parseLogService: ParseLogService,
   ) {}
 
+  @Get('')
+  async getParsedDataByDate(@Query('date') date: string) {
+    if (!date) {
+      return { message: '날짜를 YYYY-MM-DD 형식으로 입력하세요.' };
+    }
+
+    return await this.parseService.getParsedDataByDate(date);
+  }
+
   /**
    * 특정 날짜의 로그 조회
    * @param date (YYYY-MM-DD)
--- src/parse/parse.service.ts
+++ src/parse/parse.service.ts
@@ -1,15 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { JOB_NAMES, QUEUE_NAMES } from 'src/common/constants';
 import { QueueService } from 'src/common/queue.service';
+import { StockData } from 'src/entities/stock.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ParseService {
-  constructor(private readonly queueService: QueueService) {}
+  constructor(
+    private readonly queueService: QueueService,
+    @InjectRepository(StockData)
+    private stockRepository: Repository<StockData>,
+  ) {}
 
   async startParsing(): Promise<void> {
     await this.queueService.addJob(
       QUEUE_NAMES.PARSE_QUEUE,
       JOB_NAMES.PARSE_STOCK,
     );
+  }
+
+  async getParsedDataByDate(date: string) {
+    return await this.stockRepository.find({
+      where: {
+        date,
+      },
+    });
   }
 }
